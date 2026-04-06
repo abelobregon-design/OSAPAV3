cd042f3bc56cf24d4f1995c62ce5505cce064219
abelobregon-design <abel.obregon@tecsup.edu.pe>
2026-04-05 23:52:01 -0500

    Update WeaponData.cs

--- Assets/Scripts/WeaponData.cs
+++ Assets/Scripts/WeaponData.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class WeaponData
 {
-    private string weaponName;
+    private string weaponName; //VARIABLES PRIVADAS
     private int weaponDamage;
 
-    public WeaponData(string weaponName, int weaponDamage)
+    public WeaponData(string weaponName, int weaponDamage) //CONSTRUCTOR
     {
         this.weaponName = weaponName;
         this.weaponDamage = weaponDamage;
     }
 
-    public void SetWeaponName(string weaponName)
+    public void SetWeaponName(string weaponName)  //CAMBIAR VALOR
     {
         this.weaponName = weaponName;
     }
-    public void SetWeaponDamage(int weaponDamage)
+    public void SetWeaponDamage(int weaponDamage)  //CAMBIAR VALOR
     {
         this.weaponDamage = weaponDamage;
     }
 
 
-    public int WeaponDamage => weaponDamage;
-    public string WeaponName => weaponName;
+    public int WeaponDamage => weaponDamage; //PARA LECTURA DEBUG.LOG
+    public string WeaponName => weaponName; //PARA LECTURA DEBUG.LOG
 }
