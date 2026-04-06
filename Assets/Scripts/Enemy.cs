2514e0d9f6028f429d5e77abe7383aac1a849bad
abelobregon-design <abel.obregon@tecsup.edu.pe>
2026-04-05 23:51:50 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,20 +3,56 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public BaseStats stats; 
-    private void Awake()
+    private WeaponData weapon;
+    public float moveSpeed = 2f;
+    private Transform playerTarget;
+
+    private void Awake()                            //VALORES PARAMETRIZADOS CON LOS QUE NACERA EL OBJETO
     {
-        stats = new BaseStats(10, 10, 5, 1, 20);  
+        stats = new BaseStats(10, 10, 5, 1, 20);
+        weapon = new WeaponData("Cuchillo", 5);
     }
     void Start()
     {
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
 
+        if (playerObj != null)
+        {
+            playerTarget = playerObj.transform;
+        }
     }
     void Update()
     {
+        ChasePlayer();
+    }
+
+    public void ChasePlayer()
+    {
+        if (playerTarget == null) return;
+
+        transform.position = Vector2.MoveTowards(transform.position, playerTarget.position, moveSpeed * Time.deltaTime);
 
     }
-    public void TakeDamage(Player player)
+
+    public void TakeDamage(Player player)   //Metodo para recibir daño de Player
     {
-        int damage = player.Weapon.WeaponDamage;
+        int damage = player.Weapon.WeaponDamage;    //variable damage viene a ser el daño del arma de player.
+        stats.SetHealth(stats.Health - damage); //vamos a cambiar la vida local, y le restamos el daño de player
+
+        Debug.Log("Enemy recibió " +  damage + " de daño");
+        Debug.Log("Vida restante " + stats.Health);
+
+        if (stats.Health <= 0)
+        {
+            Debug.Log("Enemigo muerto");
+            Destroy(gameObject);
+        }
     }
+    public WeaponData Weapon => weapon;
+    private void OnDestroy()
+    {
+        Debug.Log("Enemigo eliminado");
+    }
+   
+   
 }
